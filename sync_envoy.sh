b931ff2fb133290385de1e090da5aaac9dbb303d
Ryan Northey <ryan@synca.io>
2023-10-02 20:05:21 +0100

    envoy/sync: Update mobile hashes
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- sync_envoy.sh
+++ sync_envoy.sh
@@ -8,9 +8,6 @@ ENVOY_VERSION=$(git -C "$ENVOY_SRC_DIR" rev-parse HEAD)
 
 echo "Syncing Envoy -> ${ENVOY_VERSION}"
 
-# bazel run //bazel:update envoy_mobile "${ENVOY_VERSION}"
-bazel run //bazel:update envoy "${ENVOY_VERSION}"
-
 if [[ -n "$COMMITTER_NAME" ]]; then
     git config --global user.name "$COMMITTER_NAME"
 fi
@@ -18,6 +15,9 @@ fi
 if [[ -n "$COMMITTER_EMAIL" ]]; then
     git config --global user.email "$COMMITTER_EMAIL"
 fi
+
+bazel run //bazel:update envoy_mobile "${ENVOY_VERSION}"
+bazel run //bazel:update envoy "${ENVOY_VERSION}"
 
 git commit versions.bzl -m "Sync Envoy @${ENVOY_VERSION}"
 
